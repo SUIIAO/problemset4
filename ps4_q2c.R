70d8f96efc66db483753b610acec42513e1f15bd
SUIIAO <43183511+SUIIAO@users.noreply.github.com>
2018-12-05 16:58:47 -0500

    Update ps4_q2c.R

--- ps4_q2c.R
+++ ps4_q2c.R
@@ -27,7 +27,7 @@ args = commandArgs(trailingOnly = TRUE)
 # functions for finding named arguments
 args_to_list = function(args){
   ind = grep('=', args)  
-  args_list = strsplit(args[ind], '=')
+  args_list = sapply(args[ind],strsplit, '=')
   names(args_list) = sapply(args_list, function(x) x[1])
   
   args_list = lapply(args_list, function(x) as.numeric(x[2]))
